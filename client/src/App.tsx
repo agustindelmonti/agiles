93f21740b132cda3dd1fdc228b89e66412c1d9fc
Agustin Delmonti <agus.delmonti@gmail.com>
2022-06-08 22:06:12 -0300

    deploy client to heroku app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">Hello wordle</div>;
 }
 
 export default App;

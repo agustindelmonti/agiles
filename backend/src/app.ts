3f0fb06d855e97348a6c579524a1c2bfec7d4499
Alejandro Navarini <anavarini@frro.utn.edu.ar>
2022-06-28 19:59:22 -0300

    Fix formatting issues

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,20 @@
 import express from "express"
 import bodyParser from "body-parser"
-import cors from 'cors';
+import cors from "cors"
 import api from "./api"
 
 const app = express()
 
-const allowedOrigins = ['http://localhost:3000', 'https://wordle-dev-client.herokuapp.com/','https://wordle-prod-client.herokuapp.com/' ];
+const allowedOrigins = [
+	"http://localhost:3000",
+	"https://wordle-dev-client.herokuapp.com/",
+	"https://wordle-prod-client.herokuapp.com/",
+]
 
 const options: cors.CorsOptions = {
-  origin: allowedOrigins
-};
-app.use(cors(options));
+	origin: allowedOrigins,
+}
+app.use(cors(options))
 app.use(bodyParser.json())
 
 app.use("/", express.static("public"))
